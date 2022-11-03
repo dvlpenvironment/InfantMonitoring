1c8ed07026acfe39a08844bf52665a19f1f607a8
cmw9957 <cmw9957@gmail.com>
2022-11-03 14:13:35 +0900

    feat: Flask main page 구현

---
+++ server.py
@@ -0,0 +1,14 @@
+import threading
+from flask import Flask, render_template
+from flask import request
+from flask import Response
+from flask import stream_with_context
+
+from stream import *
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
