587d895747f95fce008e85b91c5a70b090b5490b
cmw9957 <cmw9957@gmail.com>
2022-11-08 09:23:09 +0900

    Add : stream.py 코드 전체 클래스화

--- stream.py
+++ stream.py
@@ -7,92 +7,76 @@ import numpy as np
 from threading import Thread
 from queue import Queue
 
-# ==========전역 변수 선언 ==========
-capture = None
-thread = None
-width = 640
-height = 480
-Q = Queue(maxsize=128)
-started = False
-videoFrame = None
+class Streamer :
+    def __init__(self) :
+        if cv2.ocl.haveOpenCL() :
+            cv2.ocl.setUseOpenCL(True)
+        
+        self.capture = None
+        self.thread = None
+        self.width = 640
+        self.height = 480
+        self.Q = Queue(maxsize=128)
+        self.started = False
+        self.videoFrame = None
 
-# =============함수 정의=============
-# 카메라 시작 함수
-def runCam(src=0) :
-    global capture
-    global width
-    global height
-    global thread
-    global started
+    # 카메라 시작 함수
+    def runCam(self, src=0) :
+        self.stopCam()
 
-    if platform.system() == 'Windows' :        
-        capture = cv2.VideoCapture(src, cv2.CAP_DSHOW)
-    else :
-        capture = cv2.VideoCapture(src)
+        if platform.system() == 'Windows' :        
+            self.capture = cv2.VideoCapture(src, cv2.CAP_DSHOW)
+        else :
+            self.capture = cv2.VideoCapture(src)
+        
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+
+        if self.thread is None :
+            self.thread = Thread(target=self.updateVideoFrame, args=())
+            self.thread.daemon = False
+            self.thread.start()
+        
+        self.started = True
+
+    # 카메라 중지 기능
+    def stopCam(self) :
+        if self.capture is not None :
+            self.capture.release()
+            self.clearVideoFrame()
+
+    # Thread를 통해 영상 데이터를 실시간으로 처리하는 함수
+    def updateVideoFrame(self) :
+        while True :
+            if self.started :
+                (ret, frame) = self.capture.read()
+
+                if ret :
+                    self.Q.put(frame)
+
+    # Queue에 있는 영상 데이터를 삭제하는 함수
+    def clearVideoFrame(self) :
+        with self.Q.mutex :
+            self.Q.queue.clear()
+
+    # Queue에 있는 영상 데이터를 읽는 함수
+    def readVideoFrame(self) :
+        return self.Q.get()
+
+    # 빈 영상 데이터(검은 화면)를 나타내는 함수
+    def blankVideo(self) :
+        return np.ones(shape=[self.height, self.width, 3], dtype=np.uint8)
+
+    # 영상 데이터를 바이너리 코드로 변환하는 함수
+    def bytescode(self) :
+        if not self.capture.isOpened() :
+            frame = self.blankVideo()
+        else :
+            videoFrame = self.readVideoFrame()
+            frame = imutils.resize(videoFrame, width=int(self.width))
+        
+        return cv2.imencode('.jpg', frame)[1].tobytes()
     
-    capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-
-    if thread is None :
-            thread = Thread(target=updateVideoFrame, args=())
-            thread.daemon = False
-            thread.start()
-            
-    
-    started = True
-
-# 카메라 중지 기능
-def stopCam() :
-    global capture
-    global started
-
-    if capture is not None :
-        capture.release()
-        clearVideoFrame()
-
-# Thread를 통해 영상 데이터를 실시간으로 처리하는 함수
-def updateVideoFrame() :
-    global capture
-    global started
-    global Q
-
-    while True :
-        if started :
-            (ret, frame) = capture.read()
-
-            if ret :
-                Q.put(frame)
-
-# Queue에 있는 영상 데이터를 삭제하는 함수
-def clearVideoFrame() :
-    global Q
-
-    with Q.mutex :
-        Q.queue.clear()
-
-# Queue에 있는 영상 데이터를 읽는 함수
-def readVideoFrame() :
-    global Q
-
-    return Q.get()
-
-# 빈 영상 데이터(검은 화면)를 나타내는 함수
-def blankVideo() :
-    global width
-    global height
-    
-    return np.ones(shape=[height, width, 3], dtype=np.uint8)
-
-# 영상 데이터를 바이너리 코드로 변환하는 함수
-def bytescode() :
-    global capture
-    global width
-    global videoFrame
-
-    if not capture.isOpened() :
-        frame = blankVideo()
-    else :
-        videoFrame = readVideoFrame()
-        frame = imutils.resize(videoFrame, width=int(width))
-    
-    return cv2.imencode('.jpg', frame)[1].tobytes()
+    def __exit__(self) :
+        print('=====Streamer class exit')
+        self.capture.release()
