90be7f7e1631e469349979d58675ac3dfff95ffd
cmw9957 <cmw9957@gmail.com>
2022-11-10 10:49:38 +0900

    Add : disable the reloader

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from server import app
 
 if __name__ == '__main__':
     print('CV on')
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
     print('main close')
